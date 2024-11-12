7285e4e8d805fe0a629a8fecd104ee7a356804d5
BindonWrathbite <165421910+BindonWrathbite@users.noreply.github.com>
2024-11-12 16:30:21 -0500

    updated pandas_intro

--- basics_pandas/pandas_intro.py
+++ basics_pandas/pandas_intro.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import datasets
 
-dataset = datasets.load_dataset('/data_jobs.csv')
+dataset = datasets.load_dataset('./data_jobs.csv')
 df = dataset['train'].to_pandas()
 
 df['job_title_short']
